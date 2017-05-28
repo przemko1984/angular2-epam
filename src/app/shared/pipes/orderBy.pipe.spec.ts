6b7a83eaed1ba0fd80c38f8dca24c991ae596b29
Przemyslaw Ozog <przemyslaw_ozog@epam.com>
2017-05-28 20:58:47 +0200

    task 11: dodanie plikow bez testow z pipeami

---
+++ src/app/shared/pipes/orderBy.pipe.spec.ts
@@ -0,0 +1,14 @@
+import { OrderByPipe } from './orderBy.pipe';
+
+describe(`DurationPipe`, () => {
+    let pipe: OrderByPipe;
+
+    beforeEach(() => {
+        pipe = new OrderByPipe();
+    });
+
+    // it('transforms "0" to "0 min"', () => {
+    //     expect(pipe.transform(0)).toBe('0 min');
+    // });
+
+});
